32efdb3a3379108251ac088d0f7ad62ce056bd16
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2022-09-27 22:32:49 -0700

    Added notes.js  functions
    Added "GET" function to grab existing ntoes, added "POST" function to post existing notes, and added "delete" function to delete notes.

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,10 +1,68 @@
 const router = require("express").Router();
 const fs = require("fs");
 
-
+router.get("/", async (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) {
+      res.json(err);
+    } else {
+      const index = JSON.parse(data);
+      res.json(index);
+    }
+  });
+});
 
 // Function to "GET" notes
+router.get("./notes", async (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) {
+      res.json(err);
+    } else {
+      const notes = JSON.parse(data);
+      res.json(notes);
+    }
+  });
+});
 
 // Function to display notes
+router.post("/", async (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) {
+      res.json(err);
+    } else {
+      const newNote = req.body;
+      const notes = JSON.parse(data); // add ID here
+      notes.push(newNote);
+      fs.writeFile("./db/db.json", JSON.stringify(notes, null, "\t"), (err) => {
+        if (err) {
+          res.json(err);
+        } else {
+          res.json(notes);
+        }
+      });
+    }
+  });
+});
 
 // Function to delete notes
+router.delete("/:id", async (req, res) => {
+  if (req.body) {
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+      if (err) {
+        res.json(err);
+      } else {
+        const notes = JSON.parse(data);
+        for (let i = 0; i < notes.length; i++) {
+          const newNote = notes[i];
+          if (req.body === newNote) {
+            notes.spliced(i, 1);
+          }
+        }
+        fs.writeFile("./db.db.json", JSON.stringify(notes), (err) => {
+          err ? console.error(err) : console.log("Updated!");
+          res.json(req.body);
+        });
+      }
+    });
+  }
+});
